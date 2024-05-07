63a528aefc67e777c86503271c73c9b5febb5ad8
SoySalvaa35 <116510579+SoySalvaa35@users.noreply.github.com>
2024-05-06 22:24:11 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -98,30 +98,30 @@ export default {
                             </li>
                         </ol>
                     </template>
-                    <h3>Submission Requirements</h3>
+                    <h3>Requisito de records</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Se consiguio el record sin el uso de hacks
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Se consiguio el record en un nivel que esta en la lista
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Tener la fuente de clicks o los clicks en el video
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                       La grabacion debe tener un intento anterior y toda la animacion de muerte antes de pasartelo, en caso de pasartelo en 1 intento, toda la grabacion previa y posterior a la completicion
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        La grabacion debe mostrar al jugador entrando a la pared del final o sera invalida
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        No se admiten secret ways o bugs
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        No usar modos faciles, de ser posible usar la version original (o el LDM oficial de pointercrate o el mismo autor)
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Cuando un nivel cae en legacy, se aceptan records de el por 24 horas, tras eso, no se aceptan mas records
                     </p>
                 </div>
             </div>
